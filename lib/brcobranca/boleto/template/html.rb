07bc158a75fb9398fc51863db71f30fe077917b2
Regis Mesquita <regis.mesquita@codeminer42.com>
2012-02-17 19:15:13 -0200

    First html template try.

---
+++ lib/brcobranca/boleto/template/html.rb
@@ -0,0 +1,40 @@
+#encoding: utf-8
+
+begin
+  require 'rghost_barcode'
+rescue LoadError
+  require 'rubygems' unless ENV['NO_RUBYGEMS']
+  gem 'rghost_barcode'
+  require 'rghost_barcode'
+end
+
+module Brcobranca
+  module Boleto
+    module Template
+      # Templates para usar com HTML
+      module Html
+        extend self
+        include RGhost unless self.include?(RGhost)
+        RGhost::Config::GS[:external_encoding] = Brcobranca.configuration.external_encoding
+
+        def generate(boleto, options)
+          if boleto.is_a? Array
+            modelo_generico_multipage(boleto, options)
+          else
+            modelo_generico(boleto, options)
+          end
+        end
+
+        private
+
+        def modelo_generico(boleto, options={})
+          render :partial => 'boleto/boleto', :locals => {:boleto => boleto}
+        end
+
+        def modelo_generico_multipage(boletos, options={})
+          #TODO: Render multiples bank slips
+        end
+      end
+    end
+  end
+end
